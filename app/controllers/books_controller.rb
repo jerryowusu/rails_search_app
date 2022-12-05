3149597783c9b2af93d50ab625654f5129a1f298
Jerry Owusu <jerryowusu6133@gmail.com>
2022-12-05 22:13:59 +0000

    Create books model and controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,80 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: %i[ show edit update destroy ]
+
+  # GET /books or /books.json
+  def index
+    if params[:query].present? 
+      @books = Book.where("name LIKE ?", "%#{params[:query]}%")
+    else
+      @books = Book.all
+    end
+
+    if turbo_frame_request? 
+      render partial: 'books', locals: { books: @books }
+    else 
+      render "index" 
+    end
+  end
+
+  # GET /books/1 or /books/1.json
+  def show
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books or /books.json
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /books/1 or /books/1.json
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1 or /books/1.json
+  def destroy
+    @book.destroy
+
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:name)
+    end
+end
