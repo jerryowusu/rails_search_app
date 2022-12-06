ec88db7a8aa7fd5d840871f62dc3de98e9fab989
Jerry Owusu <jerryowusu6133@gmail.com>
2022-12-06 12:15:05 +0000

    update the edit view

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-form"
 export default class extends Controller {
   search() {
-    // console.log('great typing skills')
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
       this.element.requestSubmit()
